bc3a5eb2b80ec88ba79d303ca3bafb3a9eca6957
Ruy Monteiro <ruystfrancis@gmail.com>
2022-11-02 18:58:06 -0300

    Count endpoint + Cors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,27 @@
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function start() {
   const fastify = Fastify({
     logger: true
   });
 
-  fastify.get('/pools/count', () => {
-    return { count: 0 }
+  await fastify.register(cors, {
+    origin: true
   })
 
-  await fastify.listen({ port: 3333 })
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+    
+    return { count }
+  })
+
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 start();
